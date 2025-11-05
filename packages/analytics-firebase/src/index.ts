c10d8aabe8bfb7b1c01d6371f84dfe2994e1202d
Terry-cupist <terry@cupist.com>
2025-11-05 15:49:17 +0900

    feat(analytics-firebase): 누락 메서드 추가

--- packages/analytics-firebase/src/index.ts
+++ packages/analytics-firebase/src/index.ts
@@ -1,12 +1,2 @@
-import { IUserEventModule } from "@cupist/analytics-core";
-import { getAnalytics, logEvent } from "@react-native-firebase/analytics";
-
-export const analytics = getAnalytics();
-
-export const getFirebaseInstance: () => IUserEventModule = () => {
-  return {
-    log({ eventName, params }) {
-      logEvent(analytics, eventName, params);
-    },
-  };
-};
+export * from "./firebase";
+export * from "./user-event";
